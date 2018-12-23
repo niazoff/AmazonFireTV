832f3ba6498991847f23a7c065482b98bb34515a
Natanel Niazoff <nniazoff@icloud.com>
2018-12-23 15:14:53 -0500

    Add Amazon Fire TV capabilities

---
+++ Sources/App/AmazonFireTV.swift
@@ -0,0 +1,78 @@
+import Vapor
+
+enum AmazonFireTVApp: String {
+	case screenCloud = "screen_cloud"
+	case abc
+	
+	var packageName: String {
+		switch self {
+		case .screenCloud: return "io.screencloud.player"
+		case .abc: return "com.disney.datg.videoplatforms.android.amazon.kindle.abc"
+		}
+	}
+}
+
+struct AmazonFireTV {
+	let ipAddress: String
+	
+	struct Constants {
+		static let adbPath = "/usr/local/bin/adb"
+	}
+	
+	func connect(_ eventLoop: EventLoop) -> Future<Void> {
+		return shell(Constants.adbPath, "connect", ipAddress, eventLoop: eventLoop)
+	}
+	
+	func disconnect(_ eventLoop: EventLoop) -> Future<Void> {
+		return shell(Constants.adbPath, "disconnect", ipAddress, eventLoop: eventLoop)
+	}
+	
+	func launch(_ app: AmazonFireTVApp, eventLoop: EventLoop) -> Future<Void> {
+		return shell(Constants.adbPath, "shell", "monkey", "--pct-syskeys", "0", "-p", app.packageName, "1", eventLoop: eventLoop)
+	}
+}
+
+private func shell(_ args: String..., eventLoop: EventLoop) -> Future<Void> {
+	let promise = eventLoop.newPromise(Void.self)
+	let process = Process()
+	guard let path = args.first
+		else { promise.fail(error: ShellError.mustProvideLaunchPath); return promise.futureResult }
+	process.executableURL = URL(fileURLWithPath: path)
+	process.arguments = Array(args.dropFirst())
+	do { try process.run() } catch { promise.fail(error: error) }
+	process.terminationHandler = {
+		switch TerminationStatus($0.terminationStatus) {
+		case .succeeded: promise.succeed()
+		case .failed(let error): promise.fail(error: error)
+		}
+	}
+	return promise.futureResult
+}
+
+private enum ShellError: Error { case mustProvideLaunchPath }
+
+private enum TerminationStatus {
+	case succeeded
+	case failed(TerminationError)
+	
+	init(_ status: Int32) {
+		if status == 0 { self = .succeeded }
+		else { self = .failed(.default(status)) }
+	}
+}
+
+private enum TerminationError: Error { case `default`(Int32) }
+
+extension TerminationError: Debuggable {
+	var identifier: String {
+		switch self {
+		case .default(let status): return String(status)
+		}
+	}
+	
+	var reason: String {
+		switch self {
+		case .default(let status): return "Shell error with status: \(status)"
+		}
+	}
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,19 +2,38 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
+	router.put("launch_app") { req -> Future<HTTPStatus> in
+		return try req.content.decode(LaunchAppContent.self)
+			.thenThrowing { content in
+				let tv = AmazonFireTV(ipAddress: content.ipAddress)
+				guard let app = AmazonFireTVApp(rawValue: content.appName)
+					else { throw LaunchAppError.badAppName }
+				return tv.connect(req.eventLoop)
+					.then { tv.launch(app, eventLoop: req.eventLoop) }
+					.then { tv.disconnect(req.eventLoop) }
+					.map { return .ok }
+			}.then { return $0 }
+	}
+}
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+private struct LaunchAppContent: Content {
+	var ipAddress: String
+	var appName: String
+	
+	enum CodingKeys: String, CodingKey {
+		case ipAddress = "ip_address"
+		case appName = "app_name"
+	}
+}
+
+enum LaunchAppError: String, Error { case badAppName }
+
+extension LaunchAppError: Debuggable {
+	var identifier: String { return rawValue }
+	
+	var reason: String {
+		switch self {
+		case .badAppName: return "Must specify an available app name."
+		}
+	}
 }
