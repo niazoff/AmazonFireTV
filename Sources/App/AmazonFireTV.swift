bda15b18a25125aa4dfe569e7cdbe3f714f7fed8
Natanel Niazoff <nniazoff@icloud.com>
2018-12-23 16:40:52 -0500

    Add available

--- Sources/App/AmazonFireTV.swift
+++ Sources/App/AmazonFireTV.swift
@@ -20,18 +20,25 @@ struct AmazonFireTV {
 	}
 	
 	func connect(_ eventLoop: EventLoop) -> Future<Void> {
+		guard #available(macOS 10.13, *)
+			else { return eventLoop.newFailedFuture(error: AmazonFireTVError.notAvailableOnThisVersion) }
 		return shell(Constants.adbPath, "connect", ipAddress, eventLoop: eventLoop)
 	}
 	
 	func disconnect(_ eventLoop: EventLoop) -> Future<Void> {
+		guard #available(macOS 10.13, *)
+			else { return eventLoop.newFailedFuture(error: AmazonFireTVError.notAvailableOnThisVersion) }
 		return shell(Constants.adbPath, "disconnect", ipAddress, eventLoop: eventLoop)
 	}
 	
 	func launch(_ app: AmazonFireTVApp, eventLoop: EventLoop) -> Future<Void> {
+		guard #available(macOS 10.13, *)
+			else { return eventLoop.newFailedFuture(error: AmazonFireTVError.notAvailableOnThisVersion) }
 		return shell(Constants.adbPath, "shell", "monkey", "--pct-syskeys", "0", "-p", app.packageName, "1", eventLoop: eventLoop)
 	}
 }
 
+@available(macOS 10.13, *)
 private func shell(_ args: String..., eventLoop: EventLoop) -> Future<Void> {
 	let promise = eventLoop.newPromise(Void.self)
 	let process = Process()
@@ -48,6 +55,8 @@ private func shell(_ args: String..., eventLoop: EventLoop) -> Future<Void> {
 	}
 	return promise.futureResult
 }
+
+private enum AmazonFireTVError: Error { case notAvailableOnThisVersion }
 
 private enum ShellError: Error { case mustProvideLaunchPath }
 
